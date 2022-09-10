d9e47453f423487a5d96be42feb834fb6078d44d
dhonstudio <doon13@gmail.com>
2022-09-11 04:19:06 +0700

    testing develop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
 function App() {
+  const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {formatName(user)}
         </p>
         <a
           className="App-link"
